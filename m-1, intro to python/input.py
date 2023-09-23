cb8a72a58fee4c7ad5406ed4a3690f5c1a7a00a2
hexaalif <hexaalif2020@gmail.com>
2023-09-24 00:35:21 +0600

    variable and input

---
+++ m-1, intro to python/input.py
@@ -0,0 +1,9 @@
+print('Now I need money')
+# input()
+# input('Give me money: ')
+# money=input('give me mny: ')
+# print("here is you money ",money)
+firstMoney= input('kodom ali: ')
+secondMoney = input('bappa raj: ')
+sum=type(firstMoney)+secondMoney
+print(sum)
