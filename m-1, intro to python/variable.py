7a450aa120e3fb062ea3bedf9b2e7d8226d4bf10
hexaalif <hexaalif2020@gmail.com>
2023-09-24 00:19:06 +0600

    first commit

---
+++ m-1, intro to python/variable.py
@@ -0,0 +1,20 @@
+age= 45
+interest_rate=2.75
+name="Jhume jo pathaan"
+district= 'B baria'
+is_single=True
+is_sleeping=False
+
+print(type(is_single))
+
+# print(age)
+
+""" alt+shift+a """
+
+print(age+interest_rate)
+print(district)
+
+print('kodom ali'+' '+'kacha badam')
+
+text=f"kodom ali {age} living in {district}. with interest rate {interest_rate}"
+print(text)
