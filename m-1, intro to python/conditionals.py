3c76c3c28a25245e501b43b048a4f9ca5742b5ab
hexaalif <hexaalif2020@gmail.com>
2023-09-24 16:21:02 +0600

    conditionals in python

---
+++ m-1, intro to python/conditionals.py
@@ -0,0 +1,28 @@
+a=4
+boss=False
+if a>5:
+    print('5 er besi')
+    print('koto bshi k jne')
+elif a>3:
+    print('olpo boro')
+elif a==2:
+    print('2 er shoman')
+else:
+    print('choto choto')
+
+# if boss is True:
+#     print('lunch er por asen')
+# else:
+#     print('boss kaj hye jbe')
+if boss is not True:
+    print('lunch er por asen')
+else:
+    print('boss kaj hye jbe')
+
+coin='head'
+if coin == 'tail':
+    print('batting')
+    if 5>2 or boss != True:
+        print('do something')
+        if 8%2==0 and 5%2==1:
+            print('even')
