8023810323aa1e15e6592adce6a274ca26534a78
hexaalif <hexaalif2020@gmail.com>
2023-09-25 06:51:42 +0600

    practice in python

---
+++ m-1, intro to python/practice.py
@@ -0,0 +1,21 @@
+first= input("first no. ")
+second = input("second no. ")
+third= input("third no. ")
+
+# 3
+sum=int(first)+int(second)+int(third)
+print(sum)
+
+
+# 2
+if int(first)> int(second) and int(first)> int(third):
+    print(int(first))
+elif int(second) > int(first) and int(second)> int(third):
+    print(int(second))
+else:
+    print(third)
+
+# 4
+# run a loop and show the odd numbers between 39 to 68
+
+
