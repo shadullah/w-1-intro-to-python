1790a4faf2372747623efb52c0dddcb47424b2eb
hexaalif <shadullahsad9@gmail.com>
2023-09-28 23:13:29 +0600

    kargs

--- m-2, basic python/default_kargs.py
+++ m-2, basic python/default_kargs.py
@@ -1,5 +1,30 @@
 def full_name(first,last):
     name=f'{first} {last}'
     return name
-name= full_name('alu','kodu')
+# take paremeters in order
+# name= full_name('alu','kodu')
+
+# name= full_name(last='kodu', first='ami')
+# print(name)
+
+# def famous(**kargs)
+def famous_nm(first,last ,**addition):
+    name=f'{first} {last}'
+    # print(addition['title'])
+    for key,value in addition.items():
+        print(key,value)
+    return name
+name=famous_nm(first='Taher', last='ali', title='hujur', addition='taheriar')
 print(name)
+
+# def function_Name(num1, num2, *args, **kargs):
+
+def a_lot(num1,num2):
+    sum= num1+num2
+    mult=num1+num2
+    remain = num1-num2
+    return [sum, mult, remain]
+    # return sum, mult, remain
+
+everything= a_lot(55,22)
+print(everything)
