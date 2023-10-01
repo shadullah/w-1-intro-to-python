41381bfe4cf3cd0fc4f4396862bb9680c83f4280
hexaalif <shadullahsad9@gmail.com>
2023-10-01 23:24:35 +0600

    dictionary in python

---
+++ m-3, basic python 2/dictionary.py
@@ -0,0 +1,26 @@
+numbers = [12, 56 , 98, 78, 56, 12, 26, 98]
+person1 = ['Kala Chan', 'kalipur', 23, 'student']
+# key value pair
+# dictionary
+# object
+# hash table
+# overlap with set
+# {key: value, key: value, key: value}
+person = {'name': 'Kala Pakhi', 'address': 'Kaliapur', 'age': 23, 'job': 'facebooker'}
+print(person)
+# print(person['job'])
+print(person.keys())
+print(person.values())
+person['language']='python'
+person['name']='sada pakhi'
+del person['age']
+print(person)
+
+# for item in person:
+#     print(item)
+
+    # special dictiionary loop
+for key,value in person.items():
+    print(key, value)
+
+
