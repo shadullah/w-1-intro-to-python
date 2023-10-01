08e4f66b6bca6ae72fad5166548ed81ba2e7fe9c
hexaalif <shadullahsad9@gmail.com>
2023-10-01 23:30:43 +0600

    built in modules in python

---
+++ m-3, basic python 2/built_in_modules.py
@@ -0,0 +1,13 @@
+from math import *
+result = ceil(4.00001)
+result = floor(4.00001)
+print(result) 
+
+from random import *
+print(random())
+print(randint(1,100))
+
+
+from time import *
+sleep(4)
+print(choice(['sakib', 'mash', 'mush']))
