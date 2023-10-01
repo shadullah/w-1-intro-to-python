cce01d856b3c7f188557ad87f382d675aa292af2
hexaalif <shadullahsad9@gmail.com>
2023-10-02 01:12:24 +0600

    lambda in python

--- m-3, basic python 2/lambda.py
+++ m-3, basic python 2/lambda.py
@@ -10,4 +10,25 @@ sum = lambda x,y  : x+y
 result = doubled(44) 
 result1 = squared(44) 
 summ = sum(2,3)
-print(summ)
+# print(summ)
+
+numbers=[12,56, 98, 78, 56, 12, 6, 98]
+doubled_nums=map(doubled, numbers)
+squared_nms = map(lambda x: x*x, numbers)
+print(numbers)
+print(list(doubled_nums))
+print(list(squared_nms))
+
+
+actors = [
+    {'name': 'sabana', 'age': 65 },
+    {'name': 'sabnoor', 'age': 45 },
+    {'name': 'sabila noor', 'age': 30 },
+    {'name': 'srabonti', 'age': 38 },
+    {'name': 'shaon', 'age': 47 },
+]
+
+juniors=filter(lambda actor: actor['age']<40, actors)
+fivers=filter(lambda actor: actor['age']%5==0, actors)
+print(list(fivers))
+
