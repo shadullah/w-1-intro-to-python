443bb785aa6df278657e480a3e2211851b01e400
hexaalif <shadullahsad9@gmail.com>
2023-10-03 22:30:20 +0600

    pyautogui

--- m-3, basic python 2/autotypePYAUTOGUI.py
+++ m-3, basic python 2/autotypePYAUTOGUI.py
@@ -1,6 +1,11 @@
 import pyautogui
 from time import sleep
-sleep(5)
-for i in range(0,3):
-    pyautogui.write('I love you, Python', interval=0.25)
-    pyautogui.press('enter')
+# sleep(5)
+# for i in range(0,3):
+#     pyautogui.write('I love you, Python', interval=0.25)
+#     pyautogui.press('enter')
+
+while True:
+    pyautogui.typewrite("#")
+    sleep(2)
+    pyautogui.press("enter")
